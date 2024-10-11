ca155bb1edebcb3ddf026bbb285ea30cda0177c7
Pedro Rampinelli <pedrorampinelli41@hotmail.com>
2024-10-11 09:55:33 -0300

    Adiciona anotacoes nas classes de domain

--- src/main/java/com/api/helpdesk/domain/Tecnico.java
+++ src/main/java/com/api/helpdesk/domain/Tecnico.java
@@ -1,18 +1,26 @@
 package com.api.helpdesk.domain;
 
+import com.api.helpdesk.domain.enums.Perfil;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
+@Entity
 public class Tecnico extends Pessoa{
 
+    @OneToMany(mappedBy = "tecnico")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Tecnico() {
+        super();
+        addPerfil(Perfil.CLIENTE);
     }
 
     public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfil(Perfil.CLIENTE);
     }
 
     public List<Chamado> getChamados() {
